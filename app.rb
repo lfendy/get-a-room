fc26216708ee87fa2bb1f705b6031909c66fae07
Melbourne Delivery Team 2 <IOOFMELPlatformTeam2@ioof.com.au>
2014-10-30 16:49:39 +1100

    Added new route for app, added petar.html for testing (Petar)

--- app.rb
+++ app.rb
@@ -1,7 +1,17 @@
 require 'sinatra'
 
 get '/' do
+  # redirect to index.html
     File.read(File.join('static', 'index.html'))
+end
+
+get '/petar' do
+  # redirect to index.html
+    File.read(File.join('static', 'petar.html'))
+end
+
+get '/static' do
+  # serve stuff from /static
 end
 
 get '/agent' do
