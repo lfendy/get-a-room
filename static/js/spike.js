a0561bdabe6e0b17e61503c7de4f943f8b502419
Melbourne Delivery Team 2 <#IOOFMELPlatformTeam2@ioof.com.au>
2014-10-30 17:38:09 +1100

    applied floor plan image and give transparency

--- static/js/spike.js
+++ static/js/spike.js
@@ -2,25 +2,28 @@ var canvas = document.getElementById('roomCanvas'),
     ctx = canvas.getContext('2d');
 
 
+
 canvas.width = 600;
-canvas.height = 700;
+canvas.height = 800;
 
-bgImage = document.getElementById('background');
-ctx.drawImage(bgImage, 0, 0);
+var bgImage = new Image();
+bgImage.onload = function() {
+  ctx.drawImage(bgImage, 0, 0, 700, 900);
+  container.redraw();
+}
+bgImage.src = "resources/floorplan_big.png"
 
-var container = new CanvasLayers.Container(canvas, false);
+var container = new CanvasLayers.Container(canvas, true);
 container.onRender = function(layer, rect, context) {
 }
+
 
 var roomLayer = new CanvasLayers.Layer(40, 20, 160, 120);
 container.getChildren().add(roomLayer);
 
 roomLayer.onRender = function(layer, rect, context) {
-    context.fillStyle="#FF0000";
+    context.fillStyle='rgba(0, 0, 255, 0.5)';
     context.fillRect(layer.getX(), layer.getY(), layer.getWidth(), layer.getHeight());
 }
-
-container.redraw();
-
 
 
